4c307e3be0faca6e64adf5550ce333d08f68df20
Dennis Goldin <dennis.goldin@gmail.com>
2016-02-18 18:12:10 -0500

    Add card flip.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,18 @@
-(function () {
- 'use strict';
+$(function () {
+  'use strict';
 
- }
+  var inputs = $("input:checked");
+
+  $(".card").on("click", flip);
+
+  function flip() {
+    console.log("** flip() **");
+    $(this).toggleClass("flip");
+    $(this).children().toggleClass("showIcon");
+    // $(this).children().css({"opacity":"1",
+    //                         "transition-delay":"0.25s"});
+  }
+
+
+
+})  // End of file.
